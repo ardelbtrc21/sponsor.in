df8de7e78f8a11c81cb13ec1122bcfe7755e9b79
vnsaprilyy22 <118784135+vnsaprilyy22@users.noreply.github.com>
2025-04-23 00:34:15 +0700

    create migrations

---
+++ backend/src/migrations/20250415172014-create-milestones-table.js.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable("milestones", {
+            milestone_id: {
+                type: Sequelize.UUID,
+                defaultValue: () => uuidv4(),
+                primaryKey: true,
+                allowNull: false,
+                unique: true
+            },
+            status_id: {
+                type: Sequelize.UUID,
+                allowNull: false
+            },
+            milestone_name: {
+                type: Sequelize.TEXT,
+                allowNull: false
+            },
+            milestone_status: {
+                type: Sequelize.TEXT,
+                allowNull: false
+            },
+            created_date: {
+                type: Sequelize.DATE,
+                allowNull: false
+            },
+            completed_date: {
+                type: Sequelize.DATE,
+                allowNull: false
+            },
+            createdAt: {
+              allowNull: false,
+              type: Sequelize.DATE
+            },
+            updatedAt: {
+              allowNull: false,
+              type: Sequelize.DATE
+            },
+        });
+    },
+    down: async (queryInterface) => {
+        await queryInterface.dropTable("submission");
+    },
+};
