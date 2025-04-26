3fe3329081324de67d681b58f12cd3e248d65f32
vnsaprilyy22 <118784135+vnsaprilyy22@users.noreply.github.com>
2025-04-26 14:25:59 +0700

    add migration milestone status

---
+++ backend/src/migrations/20250426043858-create-milestone_statuses-table.js.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("milestone_statuses", {
+      milestone_status_id: {
+        type: Sequelize.UUID,
+        defaultValue: () => uuidv4(),
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+      },
+      proposal_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      status_name: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      endAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("milestone_statuses");
+  },
+};
